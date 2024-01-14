2c4cb92c794e43a1a040911a0d2dd0126ef30012
Mykhailo Zbihlei <mikeyork32@gmail.com>
2024-01-14 11:33:40 +0200

    cors issues

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,13 @@ async function startServer() {
 
   await apolloServer.start();
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://crud-frontend-peach.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
   app.use(cors({
     origin: "https://crud-frontend-peach.vercel.app",
     credentials: true
